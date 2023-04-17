29add61c7d91e610d0b3ea57ed8d6cb64294192d
yurizem <yuri.vinicius.dev333@gmail.com>
2023-04-17 10:09:07 -0300

    Adicionando servidor local json para fazer requisicoes para api

--- bytebank/src/app/services/transferencia.service.ts
+++ bytebank/src/app/services/transferencia.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
+import { Transferencia } from '../models/transferencia.model';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
 
   private listaTransferencia:any[];
+  private url = 'http://localhost:3000/transferencias';
 
-  constructor() {
-    this.listaTransferencia = []
+  constructor( private httpClient: HttpClient) {
+    this.listaTransferencia = [];
+
   }
 
   get transferencias(){
     return this.listaTransferencia;
   }
+
+  todas(): Observable<Transferencia[]>{
+    return this.httpClient.get<Transferencia[]>(this.url);
+  }
+
   adicionar( transferencia:any){
     this.hydratar(transferencia)
     this.listaTransferencia.push(transferencia);
